c0318b00490cdf54b6f97355c316260b1ef2a964
BungaaFACE <vladislav0218@gmail.com>
2023-12-12 20:53:49 +0300

    multiple ricipients for mail sub + fixed next date for send

--- email_distribution/services.py
+++ email_distribution/services.py
@@ -1,66 +1,58 @@
 from datetime import datetime, timedelta
 import logging
+import smtplib
 from django.core.mail import send_mail
 from config import settings
-from email_distribution.models import EmailSubscribtion, EmailSubscribtionLogs
-from config.settings import DEBUG
+from email_distribution.models import Client, EmailFilling, EmailSubscribtion, EmailSubscribtionLogs
 
 
 class SendEmail:
-    def __init__(self, reciever, email_template) -> None:
-        self.reciever_email = reciever.email
-        self.reciever_fullname = reciever.full_name
+    def __init__(self, client: Client, email_template: EmailFilling) -> None:
+        self.client_email = client.email
+        self.client_fullname = client.full_name
 
         self.email_subject = email_template.email_subject
         self.email_body = email_template.email_body
 
-        self.email_subject = self.email_subject.replace(
-            '@client_fullname', self.reciever_fullname)
-        self.email_subject = self.email_subject.replace(
-            '@client_email', self.reciever_email)
-
-        self.email_body = self.email_body.replace(
-            '@client_fullname', self.reciever_fullname)
-        self.email_body = self.email_body.replace(
-            '@client_email', self.reciever_email)
+        self.email_subject = self.email_subject.\
+            replace('@client_fullname', self.client_fullname).\
+            replace('@client_email', self.client_email)
+        self.email_body = self.email_body.\
+            replace('@client_fullname', self.client_fullname).\
+            replace('@client_email', self.client_email)
 
     def send_email(self):
         return send_mail(
             self.email_subject,
             self.email_body,
             settings.EMAIL_HOST_USER,
-            [self.reciever_email]
+            [self.client_email],
+            fail_silently=False
         )
 
 
 def start_distribution_task():
+    list_of_distributions = EmailSubscribtion.objects.filter(
+        is_enabled='enabled',  # Рассылка включена
+        status='idle',  # Не в работе
+        # Дата следующей отправки меньше текущей даты
+        next_send_date__lte=datetime.now(),
+        time__lte=datetime.now().time()  # Время отправки меньше текущего времени
+    )
 
-    if DEBUG:
-        from os.path import dirname, abspath, join
-        current_path = dirname(abspath(__file__))
-        logging.basicConfig(level=logging.INFO,
-                            filename=join(current_path, "send_mails_log.log"),
-                            filemode="w",
-                            format="%(asctime)s %(levelname)s %(message)s")
+    sending_errors = ''
+    for distribution in list_of_distributions:
+        distribution.status = 'in_progress'
+        distribution.save()
 
-    try:
-        list_of_distributions = EmailSubscribtion.objects.filter(
-            is_enabled='enabled',  # Рассылка включена
-            status='idle',  # Не в работе
-            # Дата следующей отправки меньше текущей даты
-            next_send_date__lte=datetime.now(),
-            time__lte=datetime.now().time()  # Время отправки меньше текущего времени
-        )
-        logging.info("Список рассылок")
-        logging.info(list_of_distributions)
+        
+        for client in distribution.client.all():
+            try:
+                sending_result = SendEmail(client, distribution.email_filling).send_email()
+            except smtplib.SMTPException as e:
+                sending_errors += f'Send for {client.email} failed. Message: {str(e.message)}; '
 
-        for distribution in list_of_distributions:
-            distribution.status = 'in_progress'
-            distribution.save()
-
-            sending_result = SendEmail(distribution.client,
-                                       distribution.email_filling).send_email()
-
+            # Если хоть одно сообщение отправилось - считаем успешным
             if sending_result:
                 match distribution.periodic_time:
                     case 'daily':
@@ -69,19 +61,20 @@ def start_distribution_task():
                         delta = timedelta(7)
                     case 'monthly':
                         delta = timedelta(30)
-                distribution.next_send_date = distribution.next_send_date + delta
+                distribution.next_send_date = datetime.now() + delta
 
-            log = EmailSubscribtionLogs()
-            log.subscription = distribution
-            log.last_try_date = datetime.now()
-            log.is_success = bool(sending_result)
-            if sending_result:
-                log.last_mail_response = 'success'
-            else:
-                log.last_mail_response = 'failure'
-            log.save()
+        log = EmailSubscribtionLogs()
+        log.subscription = distribution
+        log.last_try_date = datetime.now()
+        log.is_success = bool(sending_result)
+        logging.warning(sending_errors)
+        # Если есть smtp ошибки - добавляем их в лог
+        if sending_errors:
+            log.last_mail_response = sending_errors
+        else:
+            log.last_mail_response = 'success'
+        log.save()
 
-            distribution.status = 'idle'
-            distribution.save()
-    except Exception as e:
-        logging.critical(e, exc_info=True)
+        distribution.status = 'idle'
+        distribution.save()
+
